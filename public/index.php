9386cb689ac0371373dbb9bfe156291325aeea8b
One2r <601941036@qq.com>
2018-01-21 16:30:41 +0800

    add composer

--- public/index.php
+++ public/index.php
@@ -2,6 +2,8 @@
 
 define('APPLICATION_PATH', dirname(__FILE__)."/../");
 
+require APPLICATION_PATH . '/vendor/autoload.php';
+
 $application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
 
 $application->bootstrap()->run();
